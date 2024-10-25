7091b5282f729beb5c628327a506f18ea8f83ae6
noselenar <noselenar@gmail.com>
2024-10-26 01:50:29 +0800

    uls

---
+++ ultralytics/train.py
@@ -0,0 +1,7 @@
+import time
+
+from ultralytics import YOLO
+
+model = YOLO('yolo11m.pt')
+results = model.train(data='ultralytics/cfg/datasets/Mydata.yaml', epochs=5, imgsz=[1920, 1200], device=[0,], workers=0, batch=2 , cache=True)
+time.sleep(10)
