8e500b6dae45f96c1a2936256f36a9cc769f816c
noselenar <noselenar@gmail.com>
2024-10-26 03:00:22 +0800

    ad1

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -3,5 +3,5 @@ import time
 from ultralytics import YOLO
 
 model = YOLO('yolo11m.pt')
-results = model.train(data='ultralytics/cfg/datasets/Mydata.yaml', epochs=5, imgsz=[1920, 1200], device=[0, 1], workers=4, batch=8 , cache=True)
+results = model.train(data='ultralytics/cfg/datasets/Mydata.yaml', epochs=5, imgsz=[1920, 1200], device=[0, 1], workers=0, batch=8 , cache=True)
 time.sleep(10)
