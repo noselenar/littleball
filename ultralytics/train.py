6e9e02475b5ab27a9ed1d4035e48eedf759f2ef4
noselenar <noselenar@gmail.com>
2024-10-26 03:03:47 +0800

    ad

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -1,7 +1,18 @@
-import time
-
 from ultralytics import YOLO
 
+# 确定使用的 GPU 设备
+device = 'cuda:0,1'  # 使用两个 GPU
+
+# 加载模型
 model = YOLO('yolo11m.pt')
-results = model.train(data='ultralytics/cfg/datasets/Mydata.yaml', epochs=5, imgsz=[1920, 1200], device=[0, 1], workers=0, batch=8 , cache=True)
-time.sleep(10)
+
+# 训练模型
+results = model.train(
+    data='ultralytics/cfg/datasets/Mydata.yaml',  # 数据集路径
+    epochs=5,
+    imgsz=[1920, 1200],
+    device=device,  # 指定使用的设备
+    workers=4,      # 使用的工作进程数量
+    batch=8,        # 每个 GPU 的批量大小
+    cache=True
+)
